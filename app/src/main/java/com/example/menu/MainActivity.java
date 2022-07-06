f9fb317d539726050a54505c0d64ec32122941e3
Samson Akanbi <samson.akanbi@outlook.com>
2022-07-06 15:42:37 +0100

    Created the context mode menu.

--- app/src/main/java/com/example/menu/MainActivity.java
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -4,15 +4,54 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionMode actionMode;
+
+    private ActionMode.Callback mActionModeCallBack = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+            MenuInflater menuInflater = getMenuInflater();
+            menuInflater.inflate(R.menu.action_mode_context_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.action_mode_menu_item_1:
+                    Toast.makeText(MainActivity.this, "Action Mode Menu Item 1", Toast.LENGTH_SHORT).show();
+                    actionMode.finish();
+                    return true;
+                case R.id.action_mode_menu_item_2:
+                    Toast.makeText(MainActivity.this, "Action Mode Menu Item 2", Toast.LENGTH_SHORT).show();
+                    actionMode.finish();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode actionMode) {
+            actionMode = null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +59,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.textTitle);
+        this.registerForContextMenu(textView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                if (actionMode == null) {
+                    actionMode = startActionMode(mActionModeCallBack);
+                }
+                return true;
+            }
+        });
 
     }
 
@@ -52,5 +103,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateContextMenu(menu, v, menuInfo);
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.floating_context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_copy:
+                Toast.makeText(this, "Copy Menu Clicked.", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_cut:
+                Toast.makeText(this, "Cut Menu Clicked.", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_paste:
+                Toast.makeText(this, "Paste Menu Clicked.", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
